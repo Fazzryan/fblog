2f77db1ab1b5bbc4747472c0b30c54a13e899cd0
Fazzzryan <dinda5657@gmail.com>
2024-08-09 22:08:16 +0700

    update dashboard

--- app/Http/Controllers/be/dashboard/ConDashboard.php
+++ app/Http/Controllers/be/dashboard/ConDashboard.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers\be\dashboard;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class ConDashboard extends Controller
 {
     public function index()
     {
-        return view('backend.pages.dashboard.index');
+        $get_postingan = DB::table('tbl_postingan')
+            ->join('tbl_kategori', 'tbl_postingan.id_kategori', '=', 'tbl_kategori.id')
+            ->select('tbl_postingan.*', 'tbl_kategori.nm_kategori')
+            ->limit(5)->orderBy('id', 'asc')->get();
+        $get_kategori = DB::table('tbl_kategori')->limit(5)->orderBy('id', 'asc')->get();
+        $get_pesan = DB::table('tbl_pesan')->limit(5)->orderBy('id', 'asc')->get();
+
+        $jml_postingan = DB::table('tbl_postingan')->count();
+        $jml_kategori  = DB::table('tbl_kategori')->count();
+        $jml_pesan     = DB::table('tbl_pesan')->count();
+
+        return view(
+            'backend.pages.dashboard.index',
+            compact(
+                'get_postingan',
+                'get_kategori',
+                'get_pesan',
+                'jml_postingan',
+                'jml_kategori',
+                'jml_pesan'
+            )
+        );
     }
 }
--- resources/views/backend/pages/dashboard/index.blade.php
+++ resources/views/backend/pages/dashboard/index.blade.php
@@ -37,17 +37,151 @@
 @section('konten')
     <div class="row">
         <div class="col-md-12">
-            <div class="card border-end">
+            @include('backend.layouts.app_session')
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-lg-3">
+            <div class="card rounded-8">
                 <div class="card-body">
-                    @include('backend.layouts.app_session')
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad exercitationem facere omnis molestias totam
-                    harum qui tenetur quia similique veritatis, ab odit eius inventore reprehenderit culpa vero adipisci
-                    ullam vitae ipsum illo soluta debitis! Soluta eaque totam atque nihil. Ratione hic, molestiae a minima
-                    modi amet sed voluptate adipisci suscipit fugiat, quibusdam repellat impedit maxime nemo, molestias
-                    nobis quisquam cupiditate natus harum quam officia. Aliquid quas aperiam consequuntur autem distinctio
-                    iste quos tempore rerum accusantium iure. Amet pariatur voluptas tenetur suscipit repellendus voluptates
-                    placeat culpa, cum doloremque praesentium voluptate non in nesciunt eos? Nesciunt dicta sit, totam
-                    libero nobis ut?
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <div class="d-inline-flex align-items-center">
+                                <h2 class="text-dark mb-1 font-weight-medium">{{ $jml_postingan }}</h2>
+                            </div>
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Jumlah Postingan
+                            </h6>
+                        </div>
+                        <div class="ms-auto mt-md-3 mt-lg-0">
+                            <i data-feather="layers" class="feather-icon text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+            <div class="card rounded-8">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <h2 class="text-dark mb-1 w-100 text-truncate font-weight-medium">{{ $jml_kategori }}</h2>
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Jumlah Kategori
+                            </h6>
+                        </div>
+                        <div class="ms-auto mt-md-3 mt-lg-0">
+                            <i data-feather="box" class="feather-icon text-success"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+            <div class="card rounded-8">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <div class="d-inline-flex align-items-center">
+                                <h2 class="text-dark mb-1 font-weight-medium">{{ $jml_pesan }}</h2>
+                            </div>
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Jumlah Pesan
+                            </h6>
+                        </div>
+                        <div class="ms-auto mt-md-3 mt-lg-0">
+                            <i data-feather="message-circle" class="feather-icon text-danger"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+            <div class="card rounded-8">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <div class="d-inline-flex align-items-center">
+                                <h2 class="text-dark mb-1 font-weight-medium">...</h2>
+                            </div>
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Cooming Soon
+                            </h6>
+                        </div>
+                        <div class="ms-auto mt-md-3 mt-lg-0">
+                            <i data-feather="help-circle" class="feather-icon text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Tabel Postingan --}}
+    <div class="col-lg-12">
+        <div class="card rounded-8">
+            <div class="card-body">
+                <h4 class="mb-3 fw-medium text-dark">Tabel Postingan</h4>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="bg-light text-dark">
+                            <tr>
+                                <th>#</th>
+                                <th>Judul</th>
+                                <th>Kategori</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($get_postingan as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{!! Str::limit($item->title, 90) !!}</td>
+                                    <td>{{ $item->nm_kategori }}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" align="center">Data masih kosong</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="d-md-flex align-items-center justify-content-end">
+                    <a href="{{ route('be.postingan.list') }}"
+                        class="rounded-8 btn fs-7 btn-primary text-end fw-medium">Lihat
+                        Semua <i class="bi bi-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+        {{-- Tabel Pesan --}}
+        <div class="card rounded-8 mt-3">
+            <div class="card-body">
+                <h4 class="mb-3 fw-medium text-dark">Tabel Pesan</h4>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="bg-light text-dark">
+                            <tr>
+                                <th>#</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Pesan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($get_pesan as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $item->nm_lengkap }}</td>
+                                    <td>{{ $item->email }}</td>
+                                    <td>{!! Str::limit($item->pesan, 90) !!}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" align="center">Data masih kosong</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="d-md-flex align-items-center justify-content-end">
+                    <a href="{{ route('be.pesan.list') }}" class="rounded-8 btn fs-7 btn-primary text-end fw-medium">Lihat
+                        Semua <i class="bi bi-arrow-right"></i></a>
                 </div>
             </div>
         </div>
